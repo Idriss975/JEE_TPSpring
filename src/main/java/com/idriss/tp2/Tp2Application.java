32306559c6413d4d7f8a114ce7b68add24e61b0a
Berchil <idriss@fedora.email>
2024-03-12 14:00:55 +0000

    first commit

---
+++ src/main/java/com/idriss/tp2/Tp2Application.java
@@ -0,0 +1,37 @@
+package com.idriss.tp2;
+
+import com.idriss.tp2.dao.entities.Product;
+import com.idriss.tp2.dao.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Application implements CommandLineRunner {
+	@Autowired
+	private ProductRepository pR;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Create
+		Product Prod = new Product(1, "Sanicroix", "eye dropplets", 0.5);
+		pR.save(Prod);
+
+		//Read
+		Prod = pR.getReferenceById(1);
+		System.out.println(Prod);
+
+		// Update
+		Prod.setPrice(10);
+
+		//Delete
+		pR.delete(Prod);
+
+	}
+
+}
---
+++ src/main/java/com/idriss/tp2/dao/entities/Product.java
@@ -0,0 +1,21 @@
+package com.idriss.tp2.dao.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+}
