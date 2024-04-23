0168d9df58a6b120b57f7e227f695045f3c77941
Idriss975 <idrissberchil@fedora.email>
2024-04-23 14:44:57 +0100

    Security

--- src/main/java/org/Berchil/produit/config/UserSecurityConfig.java
+++ src/main/java/org/Berchil/produit/config/UserSecurityConfig.java
@@ -1,11 +1,14 @@
 package org.Berchil.produit.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 
 public class UserSecurityConfig {
     @Bean
